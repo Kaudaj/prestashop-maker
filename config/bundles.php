d37bcdb0f2aec1ddfa94d616be7efff7fb3be9ec
Kaudaj <info@kaudaj.com>
2021-11-06 21:19:54 +0100

    Add symfonycasts/reset-password-bundle and symfony/mailer packages

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
