e266dd62eb3831dda6d5061b041ce518c6578e31
Kaudaj <info@kaudaj.com>
2022-02-15 18:35:57 +0100

    Improve tests running performance

--- tests/php/php_syntax.sh
+++ tests/php/php_syntax.sh
@@ -1,1 +1,1 @@
-find . -type f -name '*.php' ! -path "./vendor/*" ! -path "./tests/*" -exec $1 -l -n {} \; 2>&1 | (! grep -v "No syntax errors detected")
+find src tests -type f -name '*.php' -exec $1 -l -n {} \; 2>&1 | (! grep -v "No syntax errors detected")
